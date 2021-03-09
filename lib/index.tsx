86a0c34096509b805d04d7a04314062d23c7cd66
huxc <455908283@qq.com>
2021-03-10 02:04:45 +0800

    🎨 默认导出icon

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,16 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Icon from './icon/icon';
-
-const fn: React.MouseEventHandler = (e) => {
-  console.log('fn')
-  console.log(e.target);
-}
-
-ReactDOM.render(<div>
-  <Icon
-    name="wechat"
-    className="wechat-icon"
-    onClick={fn}
-  />
-</div>, document.querySelector('#root'));
+export {default as Icon} from './icon/icon';
