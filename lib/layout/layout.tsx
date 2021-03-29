9a1e2ab5cfef0655dd6b4d565367529e52619832
huxc <455908283@qq.com>
2021-03-29 23:39:17 +0800

    🎨 判断children的type，添加hasAside样式

--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import './layout.scss';
 import { scopedClassMaker } from '../scoped-class-maker';
+import Aside from './aside';
 
 const sc = scopedClassMaker('xc-layout')
 
 interface Props extends React.HTMLAttributes<HTMLElement> {
+  children: ReactElement | Array<ReactElement>
 }
 
 const Layout: React.FunctionComponent<Props> = ({
@@ -12,8 +14,16 @@ const Layout: React.FunctionComponent<Props> = ({
   children,
   ...restProps
 }) => {
+  let hasAside = false;
+  if ((children as Array<ReactElement>).length) {
+    (children as Array<ReactElement>).map(node => {
+      if (node.type === Aside) {
+        hasAside = true;
+      }
+    })
+  }
   return (
-    <div className={sc('', {extra: className})} {...restProps}>
+    <div className={sc('', {extra: [className, hasAside && 'hasAside'].join(' ')})} {...restProps}>
       { children }
     </div>
   )
