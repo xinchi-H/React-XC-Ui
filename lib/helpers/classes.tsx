996544abeae49c31a6b106deaa01220a513ce617
huxc <455908283@qq.com>
2021-03-31 00:20:27 +0800

    🎨 重构classes

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -7,20 +7,19 @@ interface ClassToggles {
 }
 
 function scopedClassMaker(prefix: string) {
-  let result;
-  return function (name?: string | ClassToggles, options?: Options) {
-    if (typeof name === 'string' || name === undefined) {
-      result = [prefix, name].filter(Boolean).join('-')
-    } else {
-      const name2 = Object.entries(name).filter(kv => kv[1]).map(kv => kv[0])
-      result = name2.map(n => 
-        [prefix, n].filter(Boolean).join('-')
-      ).join(' ')
-    }
+  return function (name: string | ClassToggles, options?: Options) {
+    const namesObject = (typeof name === 'string' || name === undefined) ? {[name]: name} : name;
+
+    const scoped = Object.entries(namesObject).filter(kv =>
+        kv[1] !== false
+      ).map(kv => kv[0]).map(name => 
+        [prefix, name].filter(Boolean).join('-')
+      ).join(' ');
+
     if (options && options.extra) {
-      return [result, options && options.extra].filter(Boolean).join(' ')
+      return [scoped, options && options.extra].filter(Boolean).join(' ');
     } else {
-      return result;
+      return scoped;
     }
   }
 
