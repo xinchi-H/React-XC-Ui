7aa7465d5c1d0751032a239489327c9eef0dbc64
huxc <455908283@qq.com>
2021-04-21 00:07:16 +0800

    🔧 file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,11 @@ module.exports = {
      {
        test:  /\.scss$/,
        use: ['style-loader', 'css-loader', 'sass-loader']
-     } 
+     },
+     {
+       test: /\.(png|jpg|jpeg|gif)$/,
+       use: ['file-loader']
+     }
     ],
   },
 }
