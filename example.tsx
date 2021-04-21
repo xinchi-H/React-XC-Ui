b4dfd4763ce6edd2dfcbdfeacc6242a21bbd3268
huxc <455908283@qq.com>
2021-04-22 00:52:34 +0800

    💄 使用NavLink；优化菜单样式

--- example.tsx
+++ example.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import {HashRouter as Router, Route, NavLink} from 'react-router-dom';
 import IconExample from './lib/icon/icon.example';
 import ButtonExample from './lib/button.example';
 import DialogExample from './lib/dialog/dialog.example';
@@ -25,24 +25,24 @@ ReactDOM.render((
           <h2>组件</h2>
           <ul>
             <li>
-              <Link to="/icon">
+              <NavLink to="/icon">
                 Icon
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link to="/button">
+              <NavLink to="/button">
                 Button
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link to="/dialog">
+              <NavLink to="/dialog">
                 对话框
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link to="/layout">
+              <NavLink to="/layout">
                 布局
-              </Link>
+              </NavLink>
             </li>
           </ul>
         </Aside>
