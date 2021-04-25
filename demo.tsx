227f59e94fac63328642e118ee6554415cb68ef7
huxc <455908283@qq.com>
2021-04-26 00:47:41 +0800

    💄 优化代码高亮样式

--- demo.tsx
+++ demo.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Highlight, {defaultProps} from "prism-react-renderer";
+import vsDark from 'prism-react-renderer/themes/vsDark';
 import { useState } from 'react';
 
 interface Props {
@@ -9,7 +10,7 @@ interface Props {
 const Demo: React.FunctionComponent<Props> = (props) => {
   const [codeVisible, setCodeVisible] = useState(false);
   const code = (
-    <Highlight {...defaultProps} code={props.code} language="jsx">
+    <Highlight {...defaultProps} theme={vsDark} code={props.code} language="jsx">
       {({className, style, tokens, getLineProps, getTokenProps}) => (
         <pre className={className} style={style}>
           {tokens.map((line, i) => (
