a95fdb784a0255a511d23ca486e9440756a82425
huxc <455908283@qq.com>
2021-04-29 01:19:24 +0800

    🎨 重构

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -9,8 +9,7 @@ interface ClassToggles {
 const scopedClassMaker = (prefix: string) => {
   return (name: string | ClassToggles, options?: Options) => {
     let extra;
-    extra = options && Array.isArray(options.extra) ? options.extra.filter(Boolean) : options && options.extra;
-    extra = options && extra || [];
+    extra = options ? (Array.isArray(options.extra) ? options.extra.filter(Boolean) : options && options.extra) : [];
     return Object
       .entries(name instanceof Object ? name : {[name]: name})
       .filter(kv => kv[1] !== false)
