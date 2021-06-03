afc41091535814bfaf22f5d7728b0b045d23e145
鑫驰 <455908283@qq.com>
2021-06-03 17:57:11 +0800

    🎨 创建滚动条

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {HTMLAttributes} from 'react';
+import {HTMLAttributes, UIEventHandler, useEffect, useRef, useState} from 'react';
 import { scopedClassMaker } from '../helpers/classes';
 import './scroll.scss'
 import scrollbarWidth from './scrollbar-width';
@@ -10,10 +10,26 @@ const sc = scopedClassMaker('xc-scroll');
 
 const Scroll: React.FunctionComponent<Props> = (props) => {
   const {children, ...restProps} = props;
+  const [barHeight, setBarHeight] = useState(0);
+  const onScroll: UIEventHandler = (e) => {};
+  const containerRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const scrollHeight = containerRef.current!.scrollHeight;
+    const viewHeight = containerRef.current!.getBoundingClientRect().height;
+    setBarHeight(viewHeight * viewHeight / scrollHeight);
+  }, []);
   return (
     <div className={sc('')} {...restProps}>
-      <div className={sc('inner')} style={{right: -scrollbarWidth()}}>
+      <div
+        className={sc('inner')}
+        style={{right: -scrollbarWidth()}}
+        ref={containerRef}
+        onScroll={onScroll}
+      >
         {children}
+      </div>
+      <div className={sc('track')}>
+        <div className={sc('bar')} style={{height: barHeight}} />
       </div>
     </div>
   )
