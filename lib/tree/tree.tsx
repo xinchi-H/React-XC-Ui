ae1d73b5a88fd7d5aec9d95ba320b1525c109428
huxc <455908283@qq.com>
2021-06-16 20:59:23 +0800

    🎨 tree递归渲染方法和简单css

--- lib/tree/tree.tsx
+++ lib/tree/tree.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { scopedClassMaker } from '../helpers/classes';
+import './tree.scss'
 
 interface SourceDataItem {
   text: string;
@@ -10,15 +12,29 @@ interface Props {
   sourceData: SourceDataItem[]
 }
 
+
+const sc = scopedClassMaker('xc-tree');
+
+const x = (item: SourceDataItem, level = 1) => {
+  return <div key={item.value}
+              className={sc({
+                ['level-' + level]: true,
+                'item': true,
+              })}>
+    <div className={sc('text')}>
+      {item.text}  
+    </div>
+    {item.children?.map(sub => {
+      return x(sub, level +1);
+    })}
+  </div>;
+}
+
 const Tree: React.FunctionComponent<Props> = (props) => {
   return (
     <div>
       {props.sourceData.map(item => {
-        return <div key={item.value}>{item.text}
-          {item.children?.map(item2 => {
-            return <div key={item2.value}>{item2.text}</div>
-          })}
-        </div>
+        return x(item);
       })}
     </div>
   )
