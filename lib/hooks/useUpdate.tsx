3a842db0f8e3692bfbf78fff129f794bd8613060
huxc <455908283@qq.com>
2021-06-28 23:30:15 +0800

    🎨 优化useUpdate

--- lib/hooks/useUpdate.tsx
+++ lib/hooks/useUpdate.tsx
@@ -1,15 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 
 const useUpdate = (dep: boolean, fn: () => void) => {
-  const [count, setCount] = useState(0);
+  const isFirst = useRef(true);
+
   useEffect(() => {
-    setCount(x => x + 1);
-  }, [ dep ]);
-  useEffect(() => {
-    if(count > 1) {
-      fn();
+    if(isFirst) {
+      isFirst.current = false;
+      return;
     }
-  }, [count]);
+    fn();
+  }, [ dep ]);
 }
 
 export default useUpdate;
