f78e00790c497b705c8adc00fd5b1b0d00bc4771
huxc <455908283@qq.com>
2021-07-20 23:29:06 +0800

    🎨 获取当前城市

--- lib/citySelect/citySelect.tsx
+++ lib/citySelect/citySelect.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './citySelect.scss';
 import { scopedClassMaker } from '../helpers/classes';
@@ -11,7 +11,7 @@ interface Props {
 const sc = scopedClassMaker('xc-city-select');
 
 const CitySelect: React.FunctionComponent<Props> = (props) => {
-  const [dialogVisible, setDialogVisible] = useState(false);
+  const [dialogVisible, setDialogVisible] = useState(true);
   const onClick = () => {
     setDialogVisible(true);
   };
@@ -30,9 +30,40 @@ const Dialog: React.FunctionComponent<{onClose: ()=> void}> = (props) => {
       className={sc('dialog')}
       onClick={props.onClose}
     >
-      弹出内容
+      <header>
+        <span className={sc('icon')}>&lt;</span>
+        <span>选择城市</span>
+      </header>
+      <CurrentCity />
+      <h2>全部城市</h2>
+      <div className="cityIndex">ABCD....</div>
+      <div className="cityList">所有城市</div>
     </div>
   ), document.body);
+}
+
+const CurrentCity: React.FunctionComponent = () => {
+  const [city, setCity] = useState('加载中...')
+  useEffect(() => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('get', 'http://ip-api.com/json/?lang=zh-CN')
+    xhr.onload = () => {
+      const string = xhr.responseText;
+      const obj = JSON.parse(string);
+      const cityInfo = obj.city;
+      setCity(cityInfo);
+    }
+    xhr.onerror = () => {
+      setCity('未知');
+    }
+    xhr.send();
+  }, []);
+
+  return (
+    <div className="currentCity">
+      当前城市：{city}
+    </div>
+  )
 }
 
 export default CitySelect;
